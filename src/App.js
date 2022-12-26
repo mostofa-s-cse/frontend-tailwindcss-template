c05ae79b07e78cb0a27638e6211ed2380be1c53b
mostofa-s-cse <mostofa.s.cse@gmail.com>
2022-12-26 15:54:57 +0600

    create app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="text-3xl font-bold text-red-500">
+          Hello world!
+        </h1>
+
+        {/* The button to open modal */}
+        <label htmlFor="my-modal" className="btn mt-5">Open Modal</label>
+        {/* Put this part before </body> tag */}
+        <input type="checkbox" id="my-modal" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            <div className="modal-action">
+              <label htmlFor="my-modal" className="btn">Yay!</label>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
